c31d3f300dc77720081c17c57f3ae9c1a30a9bec
Hrish-1 <hrishikeshm997@gmail.com>
2021-01-14 14:44:01 +0530

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,60 @@
 import React from "react";
 import Nav from "./Navbar";
+import './Home.css';
+import Course from './components/Course.js';
+import image from './images/10088.jpg';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import cpp from './images/cpp.png';
+import java from './images/java.png';
+import nodejs from './images/nodejs.png';
+import python from './images/python.png';
+import chash from './images/chash.png';
 import { Link } from "react-router-dom";
 
 function Home() {
+    const languages = [{
+                            "name" : chash,
+                            "first" :"C-SHARP"
+                        },
+                        {
+                            "name" : cpp,
+                            "first" :"C++"
+                        },
+                        {
+                            "name" : java,
+                            "first" : "JAVA"
+                        },
+                        {
+                            "name" : nodejs,
+                            "first" : "NODEJS"
+                        },
+                        {
+                            "name" : python,
+                            "first" : "PYTHON"
+                        }
+                        ];
     return( 
-           <div>
-               <Nav />
-               <h1> THIS is HOME Page!!!</h1>
-           </div> 
-           
-    
-        
+    <div className = "container-fluid">
+        <Nav/>
+        <header className = "d-flex">
+            <img src = {image} height = "500px" width = "900px" alt = "ed"/>
+            <div className = "d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className = "font-weight-bold" style = {{"color" : "#112d4e"}}>Learn to Code <br/> for Free</h1>
+                    <p>Learn to code with our beginner-friendly tutorials and examples. Read tutorials, try examples, write programs, and learn to code.</p>
+                </div>
+            </div>    
+        </header>
+        <div className="courses-offered">
+            <p className = "font-weight-bold mx-4">COURSES OFFERED</p>
+            <div className = "d-flex flex-wrap ml-4">
+                { languages.map( lang => (
+                        <Course title = {lang.name} info = {lang.first}/>
+                ))}
+            </div>
+            
+        </div>
+    </div>
     );
 
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,30 +9,20 @@ function Nav(){
     return(
         <div classNameName="container-fluid">
             <nav className="navbar navbar-expand-lg shadow-sm rounded" style={{"backgroundColor" : "#f9f7f7"}}>
-                <Link to = "/" className="navbar-brand"><BsCodeSlash size="2em" color="#3f72af"/></Link>
+                <Link to = "/" className="navbar-brand"><BsCodeSlash size="2em" color="#112d4e"/></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav ml-auto">
                     <li className="nav-item active mx-3">
-                        <a className="nav-link" href="#" style={{"color" : "#3f72af"}}>Home</a>
+                        <a className="nav-link" href="#" style={{"color" : "#112d4e"}}>Home</a>
                     </li>
                     <li className="nav-item mx-3">
-                        <a className="nav-link" href="#" style={{"color" : "#3f72af"}}>Features</a>
+                        <a className="nav-link" href="#" style={{"color" : "#112d4e"}}>Features</a>
                     </li>
                     <li className="nav-item mx-3">
-                        <a className="nav-link" href="#" style={{"color" : "#3f72af"}}>Pricing</a>
-                    </li>
-                    <li className="nav-item dropdown mx-3">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{"color" : "#3f72af"}}>
-                       Courses
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
+                        <a className="nav-link" href="#" style={{"color" : "#112d4e"}}>Pricing</a>
                     </li>
                     </ul>
                 </div>
